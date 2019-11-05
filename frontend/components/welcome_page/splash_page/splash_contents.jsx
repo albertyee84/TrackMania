e88a23932014ebc4a1e67a0881a850812079b9e1
Albert Yee <albertyee84@gmail.com>
2019-11-05 12:12:28 -0800

    Added images and css styling on splash page

--- frontend/components/welcome_page/splash_page/splash_contents.jsx
+++ frontend/components/welcome_page/splash_page/splash_contents.jsx
@@ -4,14 +4,27 @@ class SplashContents extends React.Component{
 
     render(){
         return(
-            <div>
+            <div className="splash">
                 <h1>TrackMania is changing how teams build software-one story at a time</h1>
                 <h2>Proven project management for successful teams</h2>
                 <p>With a shared view of team priorities, a process that fosters collaboration, and dynamic tools to analyze progress, your team will deliver more frequently and consistently.</p>
-                <h2>Better organization to get focused</h2>
-                <p>Keep your team on the rails. Tracker's shared backlog makes priorities clear so the team can stay organized. Easily visualize scope, focus your teamwork, and stay nimble when circumstances change.</p>
-                <h2>Tools to help you adapt and evolve</h2>
-                <p>Get more work done, more often. Tracker's guided iteration planning helps you break down and prioritize projects into manageable chunks so the team can keep the momentum toward delivering.</p>
+
+                <div className='img1'>
+                    <div className="img-elements">
+                        <h2>Better organization to get focused</h2>
+                        <br/>
+                        <p>Keep your team on the rails. Tracker's shared backlog makes priorities clear so the team can stay organized. Easily visualize scope, focus your teamwork, and stay nimble when circumstances change.</p>
+                    </div>
+                    <h1 className='splash-img1'></h1>
+                </div>
+                <div className='img1'>
+                    <h1 className='splash-img2'></h1>
+                    <div className="img-elements">
+                        <h2>Tools to help you adapt and evolve</h2>
+                        <br/>
+                        <p>Get more work done, more often. Tracker's guided iteration planning helps you break down and prioritize projects into manageable chunks so the team can keep the momentum toward delivering.</p>
+                    </div>
+                </div>
                 <h2>Team transparency at a glance</h2>
                 <p>No more surprises. With a shared, clear view of your team's work, everyone has a real-time, single source of truth. A quick scan explains your team's status, who's responsible for what, and what's coming next.</p>
                 <h2>A better way to develop</h2>
