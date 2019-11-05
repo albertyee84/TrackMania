4686cf171c0f63b0f2fbf693c797724373bb61cc
Albert Yee <albertyee84@gmail.com>
2019-11-04 20:24:30 -0800

    added gemfile info

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-ui
-//= require rails-ujs
+//= require jquery-ujs
+
 //= require activestorage
 //= require turbolinks
 //= require_tree .
