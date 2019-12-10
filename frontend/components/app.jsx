88fea7d5d10ed3fcd2829cf600e4a825b67f2746
Albert Yee <albertyee84@gmail.com>
2019-12-10 14:50:25 -0800

    will add favicon

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-
 import { Link, Switch, Redirect } from 'react-router-dom';
-
 import { AuthRoute } from '../util/route_util';
 import {ProtectedRoute} from '../util/protected_util';
-
 import Navbar from './welcome_page/navbar/navbar_container';
 import SplashContainer from './welcome_page/splash_page/splash_container';
 import ProjectsContainer from './projects/projects_container';
 import Modal from './modal/modal';
 import loggedinNavbarContainer from './welcome_page/navbar/loggedinNavbar_container';
 import StoryIndex from './story/story_index_container';
+
 
 
 const App = () => {
