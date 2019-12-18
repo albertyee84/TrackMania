332aa3a24778a54301ef2ce78e13687cd6aa9cf9
Albert Yee <albertyee84@gmail.com>
2019-12-18 10:29:41 -0800

    add hook code to story index form but did not deploy

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -2602,7 +2602,68 @@ function (_React$Component) {
   }]);
 
   return StoryIndexForm;
-}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
+}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component); // const StoryIndexForm = props => {
+//     let [name, setName] = useState("")
+//     let [description, setDescription] = useState("")
+//     let [labels, setLabels] = useState("")
+//     let [status, setStatus] = useState(props.status)
+//     let [requestor_id, setRequestorId] = useState(props.requestorId)
+//     let [project_id, setProjectId] = useState(props.projectId)
+//     const handleChange = type => {
+//         switch(type) {
+//             case "name":
+//                 setName(event.currentTarget.value)
+//             case "description":
+//                 setDescription(event.currentTarget.value)
+//             case "labels":
+//                 setLabels(event.currentTarget.value)
+//             default:
+//                 null;
+//         }
+//     };
+//     const handleSubmit = e => {
+//         e.preventDefault();
+//         props.createStory({
+//             name: name,
+//             description: description,
+//             labels: labels,
+//             status: status,
+//             requestor_id: requestor_id,
+//             project_id: project_id
+//         })
+//             setName("")
+//             setDescription("")
+//             setLabels("")
+//     };
+//     return (
+//         <form onSubmit={handleSubmit} className="AddStoryForm">
+//             <label>Name
+//                     <input
+//                     type="text"
+//                     className="storyinput"
+//                     onChange={handleChange('name')}
+//                     value={name} />
+//             </label>
+//             <label>Description
+//                     <input
+//                     type="text"
+//                     className="storyinput"
+//                     onChange={handleChange('description')}
+//                     value={description} />
+//             </label>
+//             <label>Label
+//                     <input
+//                     type="text"
+//                     className="storyinput"
+//                     onChange={handleChange('labels')}
+//                     value={labels} />
+//             </label>
+//             <input type="submit" value="Add Story" />
+//         </form>
+//     );
+// };
+// export default StoryIndexForm;
+
 
 
 
--- frontend/components/story/story_index_form.jsx
+++ frontend/components/story/story_index_form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default class StoryIndexForm extends React.Component{
     constructor(props){
@@ -61,3 +61,69 @@ export default class StoryIndexForm extends React.Component{
         );
     }
 }
+
+// const StoryIndexForm = props => {
+//     let [name, setName] = useState("")
+//     let [description, setDescription] = useState("")
+//     let [labels, setLabels] = useState("")
+//     let [status, setStatus] = useState(props.status)
+//     let [requestor_id, setRequestorId] = useState(props.requestorId)
+//     let [project_id, setProjectId] = useState(props.projectId)
+
+//     const handleChange = type => {
+//         switch(type) {
+//             case "name":
+//                 setName(event.currentTarget.value)
+//             case "description":
+//                 setDescription(event.currentTarget.value)
+//             case "labels":
+//                 setLabels(event.currentTarget.value)
+//             default:
+//                 null;
+//         }
+//     };
+
+//     const handleSubmit = e => {
+//         e.preventDefault();
+//         props.createStory({
+//             name: name,
+//             description: description,
+//             labels: labels,
+//             status: status,
+//             requestor_id: requestor_id,
+//             project_id: project_id
+//         })
+//             setName("")
+//             setDescription("")
+//             setLabels("")
+//     };
+
+//     return (
+//         <form onSubmit={handleSubmit} className="AddStoryForm">
+//             <label>Name
+//                     <input
+//                     type="text"
+//                     className="storyinput"
+//                     onChange={handleChange('name')}
+//                     value={name} />
+//             </label>
+//             <label>Description
+//                     <input
+//                     type="text"
+//                     className="storyinput"
+//                     onChange={handleChange('description')}
+//                     value={description} />
+//             </label>
+//             <label>Label
+//                     <input
+//                     type="text"
+//                     className="storyinput"
+//                     onChange={handleChange('labels')}
+//                     value={labels} />
+//             </label>
+//             <input type="submit" value="Add Story" />
+//         </form>
+//     );
+// };
+
+// export default StoryIndexForm;
